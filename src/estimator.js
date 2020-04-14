400752188bede376a352c4affe74e37c61501313
Chinomso Onyeukwu <40630918+2besweet@users.noreply.github.com>
2020-04-14 21:27:38 +0100

    smart change

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,34 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => 
 
+{
+    const input = data:
+    return {
+    data: input.
+    region: {
+        name: "Africa",
+        avgAge; 19.7,
+        avgDailyIncomeInUSD; 5,
+        avgDailyIncomePopulation; 0.71
+
+    }
+    periodType: "days",
+    timeToElapse; 58,
+    reportedCases; 674,
+    population; 66622705,
+    totalHospitalBeds; 1380614
+
+    };
+
+    impact: {
+        currentlyInfected: 6740,
+        infectionsByRequestedTime; 3533701120,
+        severeCasesByRequestedTime; 530055168,
+        totalHospitalBeds; -529157768,
+        casesForICUByRequestedTime; 176685056,
+        casesForVentilatorsByRequestedTime; 70674023,
+        dollarsInFlight; 727589060608
+    };
+
+    };
+};
 export default covid19ImpactEstimator;
